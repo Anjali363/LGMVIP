cf3e476057e12bb60cf25daf327fab49216c8abd
Anjali Shinde <45762858+Anjali363@users.noreply.github.com>
2021-10-23 13:04:01 +0530

    Add files via upload

---
+++ task2-LGMVIP/src/App.js
@@ -0,0 +1,54 @@
+
+import './App.css';
+import Users from "./components/cards";
+import React,{Component} from "react";
+
+
+    class App extends Component {
+      constructor(props) {
+        super(props);
+    
+        // Set initial state
+        this.state = { users_data: [], loading: false };
+    
+        this.updateState = this.updateState.bind(this);
+      }
+    
+      updateState() {
+        this.setState({ loading: true });
+    
+        setTimeout(
+          async function () {
+            const response = await fetch("https://reqres.in/api/users?page=1");
+            const jsonresponse = await response.json();
+            console.log(jsonresponse);
+            this.setState({ users_data: jsonresponse["data"], loading: false });
+          }.bind(this),
+          2000
+        );
+      }
+    
+      render() {
+        return (
+          <>
+            <nav className="navbar">
+              <div className="navitems">
+                <h2><span>SHINDE</span>ENTERPRISES</h2>
+                <button className="fetchbtn" onClick={this.updateState}>
+                  Get Users
+                </button>
+              </div>
+            </nav>
+            <div className="userdatacontainer">
+              <Users loading={this.state.loading} users={this.state.users_data} />
+            </div>
+            <footer className="footer">
+              <h1>Created By <a href="https://www.linkedin.com/in/anjali-shinde-84a4221a9/" target="_blank">Anjali Shinde</a> | All Rights Reserved! </h1>
+            </footer>
+          </>
+        );
+      }
+    }
+
+
+export default App;
